e7898ed5ddd88d61f591bcf81ae8461dc5fb205b
natsukbd <natsuki.pc@gmail.com>
2020-04-06 21:37:14 +0900

    リスト6.3 : ユーザのドメインサービス リスト6.4 : 永続化・再構築のためのリポジトリを用意する

---
+++ laravel/src/ApplicationService/UserRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IntroductionDDD\ApplicationService;
+
+/**
+ * Interface UserRepositoryInterface
+ * @package IntroductionDDD\ApplicationService
+ */
+interface UserRepositoryInterface
+{
+    public function findByUserId(UserId $id): User;
+
+    public function findByUserName(UserName $name): User;
+
+    public function save(User $user): void;
+
+    public function delete(User $user): void;
+}
---
+++ laravel/src/ApplicationService/UserService.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IntroductionDDD\ApplicationService;
+
+/**
+ * Class UserService
+ * @package IntroductionDDD\ApplicationService
+ */
+final class UserService
+{
+    private UserRepositoryInterface $userRepository;
+
+    /**
+     * UserService constructor.
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function exists(User $user): bool
+    {
+        $duplicatedUser = $this->userRepository->findByUserName($user->getName());
+        return $duplicatedUser !== null;
+    }
+}

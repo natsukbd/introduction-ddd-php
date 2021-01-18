ce7c04c2a233a50130b2fd644c0620ab05c3b59c
natsukbd <natsuki.pc@gmail.com>
2021-01-18 19:42:39 +0900

    リスト11.1 : サークルの識別子となる値オブジェクト

---
+++ laravel/src/Chapter11/CircleId.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace IntroductionDDD\Chapter11;
+
+final class CircleId
+{
+    /**
+     * CircleId constructor.
+     * @param string $value
+     */
+    public function __construct(private string $value)
+    {
+    }
+
+    public function value(): string
+    {
+        return $this->value;
+    }
+}
